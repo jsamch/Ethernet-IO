1a3d00076b73be02035a6d9e2bed114aaadd682f
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2012-05-28 14:19:46 -0400

    adc and quad gui elements added... look a bit weird right now

--- client/python/etherio_gui.py
+++ client/python/etherio_gui.py
@@ -22,14 +22,41 @@ class EIOCentralWidget(QWidget):
         super(EIOCentralWidget, self).__init__(parent)
 
         # widgets which will be contained in the central widget
+        self.dacFrame = QFrame() #QGroupBox("DACs")
+        self.dacFrame.setFrameStyle(QFrame.StyledPanel | QFrame.Plain)
         self.dac = [DACGroupBox("DAC %d" % i, 0.0) for i in range(8)]
+        
+        self.adcFrame = QFrame() #QGroupBox("ADCs")
+        self.adcFrame.setFrameStyle(QFrame.StyledPanel | QFrame.Plain)
+        self.adc = [ADCGroupBox("ADC %d" % i) for i in range(8)]
+        
+        self.quadFrame = QFrame() #QGroupBox("Quadratures")
+        self.quadFrame.setFrameStyle(QFrame.StyledPanel | QFrame.Plain)
+        self.quad = [QuadGroupBox("Quad %d" % i) for i in range(8)]
 
         # layout of the widgets
-        layout = QGridLayout()
+        centralLayout = QVBoxLayout()
+        centralLayout.setSpacing(0)
+        centralLayout.addWidget(self.dacFrame)
+        centralLayout.addWidget(self.adcFrame)
+        centralLayout.addWidget(self.quadFrame)
+        
+        dacLayout = QGridLayout()
         for i in range(len(self.dac)):
-            layout.addWidget(self.dac[i], 0, i)
+            dacLayout.addWidget(self.dac[i], 0, i)
 
-        self.setLayout(layout)
+        adcLayout = QGridLayout()
+        for i in range(len(self.adc)):
+            adcLayout.addWidget(self.adc[i], 0, i)
+
+        quadLayout = QGridLayout()
+        for i in range(len(self.quad)):
+            quadLayout.addWidget(self.quad[i], 0, i)
+        
+        self.setLayout(centralLayout)
+        self.dacFrame.setLayout(dacLayout)
+        self.adcFrame.setLayout(adcLayout)
+        self.quadFrame.setLayout(quadLayout)
 
         # signals
 
@@ -41,8 +68,9 @@ class DACGroupBox(QGroupBox):
         # DAC value
         self.value = value
         
-        # group box properites
+        # group box properties
         # self.setCheckable(True)
+        #self.setDisabled(True)
 
         # widgets in the box
         self.DACSlider = QSlider(Qt.Vertical)
@@ -68,6 +96,56 @@ class DACGroupBox(QGroupBox):
         layout = QVBoxLayout()
         layout.addWidget(self.DACSlider, 0, Qt.AlignHCenter)
         layout.addWidget(self.DACText)
+
+        self.setLayout(layout)
+
+class ADCGroupBox(QGroupBox):
+    
+    def __init__(self, parent=None, name="ADC #"):
+        super(ADCGroupBox, self).__init__(parent)
+
+        # ADC value
+        self.value = 0.0;
+
+        # group box properties
+        
+        # widgets
+        self.ADCText = QLineEdit()
+
+        # line settings
+        self.ADCText.setReadOnly(True)
+        
+        self.validator = QDoubleValidator(-10.0, 10.0, 4, self)
+        self.validator.setNotation(QDoubleValidator.StandardNotation)
+        self.ADCText.setValidator(self.validator)
+
+        self.ADCText.setPlaceholderText("%6.4f" % self.value)
+
+        # layout
+        layout = QGridLayout()
+        layout.addWidget(self.ADCText, 0, 0, Qt.AlignHCenter)
+
+        self.setLayout(layout)
+
+class QuadGroupBox(QGroupBox):
+
+    def __init__(self, parent=None, name="Quad #"):
+        super(QuadGroupBox, self).__init__(parent)
+
+        # Quad value
+        self.value = 0.0;
+
+        # group box properties
+        
+        # widgets
+        self.QuadText = QLineEdit()
+
+        # line settings
+        self.QuadText.setReadOnly(True)
+
+        # layout
+        layout = QGridLayout()
+        layout.addWidget(self.QuadText, 0, 0, Qt.AlignHCenter)
 
         self.setLayout(layout)
 
